25073f2bbf87e5af34ba601e501805fa426d9b1a
RizkyArifNur <rizkyarifnur@gmail.com>
2020-11-19 20:26:26 +0700

    add example of heavy component

--- src/Components/PureComponent.js
+++ src/Components/PureComponent.js
@@ -1,8 +1,48 @@
 import React from "react";
 class PureComponent extends React.PureComponent {
+  state = {
+    list: [],
+  };
+
+  componentDidMount() {
+    let newMockedList = [];
+    for (let index = 0; index < 1000; index++) {
+      newMockedList.push(index + 1);
+    }
+    this.setState({
+      list: newMockedList,
+    });
+  }
+
   render() {
     console.log("Pure component render");
-    return <div>Regular component {this.props.name} </div>;
+    return (
+      <div>
+        Pure component {this.props.name}
+        <br />
+        {this.state.list.map((el, index) => (
+          <div key={index}>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <div>
+                        <div>
+                          <div>
+                            <div> {index + 1} Mock List</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
--- src/Components/RegularComponent.js
+++ src/Components/RegularComponent.js
@@ -1,8 +1,48 @@
 import React from "react";
 class RegularComponent extends React.Component {
+  state = {
+    list: [],
+  };
+
+  componentDidMount() {
+    let newMockedList = [];
+    for (let index = 0; index < 1000; index++) {
+      newMockedList.push(index + 1);
+    }
+    this.setState({
+      list: newMockedList,
+    });
+  }
+
   render() {
     console.log("Regular component render");
-    return <div>Regular component {this.props.name} </div>;
+    return (
+      <div>
+        Regular component {this.props.name}
+        <br />
+        {this.state.list.map((el, index) => (
+          <div key={index}>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <div>
+                        <div>
+                          <div>
+                            <div> {index + 1} Mock List</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
