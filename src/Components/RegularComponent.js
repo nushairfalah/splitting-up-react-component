0c5b34d9ee3ee272d8324250560b61f17863dda0
RizkyArifNur <rizkyarifnur@gmail.com>
2020-11-19 20:09:38 +0700

    fix component naming

--- src/Components/RegularComponent.js
+++ src/Components/RegularComponent.js
@@ -1,9 +1,9 @@
 import React from "react";
-class PureComponent extends React.Component {
+class RegularComponent extends React.Component {
   render() {
     console.log("Regular component render");
     return <div>Regular component {this.props.name} </div>;
   }
 }
 
-export default PureComponent;
+export default RegularComponent;
