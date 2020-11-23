9d5cde9d71bb30edabc35f5b4c50296c7f69a970
falah <nushairfalahizz@gmail.com>
2020-11-23 21:23:23 +0700

    counter-button

--- src/App.js
+++ src/App.js
@@ -1,46 +1,73 @@
 import React from "react";
 import "./App.css";
 
-/**
- * TODO: Please import the `RegularComponent` and `PureComponent`
- */
-import PureComponent from './Components/PureComponent';
-import RegularComponent from './Components/RegularComponent';
+// /**
+//  * TODO: Please import the `RegularComponent` and `PureComponent`
+//  */
+// import PureComponent from './Components/PureComponent';
+// import RegularComponent from './Components/RegularComponent';
+
+// class App extends React.Component {
+//   /**
+//    * TODO: Please add a state to this component, the key is `name` and the initial value is `John`
+//    */
+//   state = {
+//     name: 'John',
+//     count: 0
+//   }
+
+//   clickHandler = () => {
+//     this.setState({
+//       name: 'John',
+//       count: this.state.count + 1,
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <button
+//           // TODO: Please update the value of state `name` to `John`
+//           onClick={this.clickHandler}
+//         >
+//           Update state
+//         </button>
+//         <p>{this.state.count}</p>
+//         {/* 
+//           TODO: Please attach the `RegularComponent` and `PureComponent` here
+//         */}
+//         <PureComponent name={this.state.name} />
+//         <RegularComponent name={this.state.name} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import Counter from './Components/Counter'
 
 class App extends React.Component {
-  /**
-   * TODO: Please add a state to this component, the key is `name` and the initial value is `John`
-   */
+
   state = {
-    name: 'John',
-    count: 0
+    showHide: false,
   }
 
-  clickHandler = () => {
+  showHideClick = () => {
     this.setState({
-      name: 'John',
-      count: this.state.count + 1,
+      showHide: !this.state.showHide,
     })
   }
 
   render() {
     return (
       <div className="App">
-        <button
-          // TODO: Please update the value of state `name` to `John`
-          onClick={this.clickHandler}
-        >
-          Update state
-        </button>
-        <p>{this.state.count}</p>
-        {/* 
-          TODO: Please attach the `RegularComponent` and `PureComponent` here
-        */}
-        <PureComponent name={this.state.name} />
-        <RegularComponent name={this.state.name} />
+        <button onClick={this.showHideClick}>{this.state.showHide ? "Hide Counter" : "Show Counter"}</button>
+        {this.state.showHide && <Counter />}
       </div>
-    );
+    )
   }
+
 }
 
 export default App;
--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,44 +1,86 @@
-import React from "react";
-let timeout;
+// import React from "react";
+// let timeout;
+// class Counter extends React.Component {
+//   state = {
+//     count: 0,
+//     loading: true,
+//     timeOut: undefined,
+//   };
+
+//   componentDidMount() {
+//     console.log("component mounted");
+//     // this is just an example of loading content
+//     timeout = setTimeout(() => {
+//       this.setState({
+//         loading: false,
+//       });
+//       console.log("data updated");
+//     }, 1000);
+//   }
+
+//   componentDidUpdate() {
+//     console.log("component updated");
+//   }
+
+//   componentWillUnmount() {
+//     console.log("component unmounted");
+//     clearTimeout(timeout);
+//   }
+
+//   render() {
+//     if (this.state.loading) {
+//       return <div>Loading...</div>;
+//     }
+//     return (
+//       <div>
+//         <h6> Counter </h6>
+//         <p> current count: {this.state.count} </p>
+//         <button onClick={() => this.setState({ count: this.state.count + 1 })}>increment the count</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
+
+import React from 'react';
+
 class Counter extends React.Component {
+
   state = {
     count: 0,
-    loading: true,
-    timeOut: undefined,
-  };
-
-  componentDidMount() {
-    console.log("component mounted");
-    // this is just an example of loading content
-    timeout = setTimeout(() => {
-      this.setState({
-        loading: false,
-      });
-      console.log("data updated");
-    }, 1000);
   }
 
-  componentDidUpdate() {
-    console.log("component updated");
+  plusOne = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
   }
 
-  componentWillUnmount() {
-    console.log("component unmounted");
-    clearTimeout(timeout);
+  minusOne = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
   }
 
   render() {
-    if (this.state.loading) {
-      return <div>Loading...</div>;
+    if (this.state.count < 0) {
+      this.setState({
+        count: 0
+      })
     }
     return (
       <div>
-        <h6> Counter </h6>
-        <p> current count: {this.state.count} </p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>increment the count</button>
+        <br />
+        <button onClick={this.minusOne}>&nbsp;-&nbsp;</button>
+        &nbsp;&nbsp;
+        <span>{this.state.count}</span>
+        &nbsp;&nbsp;
+        <button onClick={this.plusOne}>&nbsp;+&nbsp;</button>
       </div>
-    );
+    )
   }
+
 }
 
 export default Counter;
