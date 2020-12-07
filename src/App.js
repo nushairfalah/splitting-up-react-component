3f7a526d3e7bf2ebd8d908d09129ae836ebbcb86
RizkyArifNur <rizkyarifnur@gmail.com>
2020-12-07 22:41:10 +0700

    add example react router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import "./App.css";
-import PureComponent from "./Components/PureComponent";
-import RegularComponent from "./Components/RegularComponent";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./Pages/Home";
 class App extends React.Component {
-  state = {
-    name: "John",
-    count: 0,
-  };
-
   render() {
-    console.log("*** Parent component render ****");
     return (
       <div className="App">
-        <p> {this.state.count} </p>
-        <button onClick={() => this.setState({ name: "John", count: this.state.count + 1 })}>Update state</button>
-        <RegularComponent name={this.state.name} />
-        <PureComponent name={this.state.name} />
+        <Router>
+          <Route path="/home" component={HomePage} />
+          <Route path="/profile" component={HomePage} />
+        </Router>
       </div>
     );
   }
